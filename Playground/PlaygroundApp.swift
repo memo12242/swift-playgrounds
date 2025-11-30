087393d513fb6e4c5b3f3e4042b2c77c8f313f71
Madoka <madoka3464@gmail.com>
2025-11-30 16:10:43 +0900

    Initial Commit

---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Madoka Suzuki on 2025/11/30.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
