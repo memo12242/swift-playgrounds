e4e200634cf96ca318c61d8b1bab810c2d8d4f1d
Madoka <madoka3464@gmail.com>
2025-12-23 17:13:46 +0900

    ControlGroupViewを追加

---
+++ Playgrounds/Modules/ControlGroupView.swift
@@ -0,0 +1,22 @@
+//
+//  ControlGroupView.swift
+//  Playgrounds
+//
+//  Created by Madoka Suzuki on 2025/12/23.
+//
+
+import SwiftUI
+
+struct ControlGroupView: View {
+    var body: some View {
+        ControlGroup {
+            
+        } label: {
+            
+        }
+    }
+}
+
+#Preview {
+    ControlGroupView()
+}
